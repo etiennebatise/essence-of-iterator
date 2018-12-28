7c524a314a4b5f1194044461d3912451852fb428
Etienne Batise <etienne.batise@gmail.com>
2018-12-28 15:41:50 +0100

    Add 2.3 Monadic map

--- iterator.hs
+++ iterator.hs
@@ -2,7 +2,7 @@
 -- 2.1 Origami
 
 class Bifunctor s where
-  -- Should satisfy :
+  -- should satisfy these laws:
   -- bimap id id = id -- identity
   -- bimap (f . h) (g . k) = bimap f g ◦ bimap h k -- composition
   bimap :: (a -> b) -> (c -> d) -> s a c -> s b d
@@ -21,3 +21,18 @@ unfoldFix f  = In . bimap id (unfoldFix f) . f
 
 
 -- 2.2 Crush
+
+-- 2.3 Monadic map
+class Functor f where
+  -- should satisfy these laws:
+  -- fmap id = id -- identity
+  -- fmap (f . g) = fmap f . fmap g -- composition
+  fmap :: (a -> b) -> f a -> f b
+
+class Functor m => Monad m where
+  -- should satisfy these laws:
+  -- return a >>= f = f a -- left unit
+  -- mx >>= return = mx -- right unit
+  -- (mx >>= f) >>= g = mx >>= (\x -> f x >>= g) -- associativity
+  >>= :: m a -> (a -> m b) -> m b
+  return :: a -> m a
